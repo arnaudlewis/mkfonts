cd4c83ccd6c1214d4325e619748589aae09f398b
Denis Mludek <denis.mludek@supinfo.com>
2016-01-24 02:42:11 +0100

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
 var gulp = require("gulp");
 var iconfont = require('gulp-iconfont');
 var fontName = 'dreamact';
+var runTimestamp = Math.round(Date.now()/1000);
 
 gulp.task('fonts', function(){
   gulp.src(['icons/*.svg'])
     .pipe(iconfont({
       fontName: fontName,
-      appendCodepoints: true,
-      normalize: true,
-      svg: true
+      appendUnicode: true,
+      formats: ['ttf', 'eot', 'woff','svg'],
+      timestamp: runTimestamp
     }))
-      .on('codepoints', function(codepoints, options) {
-        console.log(codepoints, options);
+      .on('glyphs', function(glyphs, options) {
+        // CSS templating, e.g. 
+        console.log(glyphs, options);
       })
     .pipe(gulp.dest('fonts/'));
 });
