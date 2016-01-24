d69163de78569a9f3cdefbc20874b59dea6e66bc
alw <alw@zengularity.com>
2016-01-24 02:17:29 +0100

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 var gulp = require("gulp");
 var iconfont = require('gulp-iconfont');
-var fontName = 'startkit';
+var fontName = 'dreamact';
 
 gulp.task('fonts', function(){
   gulp.src(['icons/*.svg'])
     .pipe(iconfont({
       fontName: fontName,
       appendCodepoints: true,
-      normalize: true
+      normalize: true,
+      svg: true
     }))
       .on('codepoints', function(codepoints, options) {
         console.log(codepoints, options);
