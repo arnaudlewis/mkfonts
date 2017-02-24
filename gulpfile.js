edc8ff436c179f059897dee999c4db7c0f47d7bd
Arnaud lewis <alw@zengularity.com>
2017-02-24 16:44:47 +0100

    update conf

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 var gulp = require("gulp");
 var iconfont = require('gulp-iconfont');
-var fontName = 'dreamact';
+var fontName = 'prismic-website';
 var runTimestamp = Math.round(Date.now()/1000);
 
 gulp.task('fonts', function(){
   gulp.src(['icons/*.svg'])
     .pipe(iconfont({
       fontName: fontName,
-      appendUnicode: true,
+      prependUnicode: true,
       formats: ['ttf', 'eot', 'woff','svg'],
       timestamp: runTimestamp
     }))
       .on('glyphs', function(glyphs, options) {
-        // CSS templating, e.g. 
+        // CSS templating, e.g.
         console.log(glyphs, options);
       })
     .pipe(gulp.dest('fonts/'));
